6cdbdb249349279a199e1bddf32f88b877713841
aniket-dudhe <aniketdudhe62@gmail.com>
2025-11-18 12:49:32 +0530

    Add Jenkins pipeline with pull, build, test, and deploy stages

---
+++ pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    stages {
+        stage('pull-stage') {
+            steps {
+                echo "git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'"
+            }
+        }
+        stage('Build') {
+            steps {
+                echo " build sucess"
+            }
+        }
+        stage('test') {
+            steps {
+                echo "test suecss"
+            }
+        }
+        stage('deploy') {
+            steps {
+                echo "deploy sucess"
+            }
+        }
+    }
+}
