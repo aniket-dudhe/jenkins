f6851605d8037dec3f68eaf377c6b81f21747008
aniket-dudhe <aniketdudhe62@gmail.com>
2025-11-19 20:03:20 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('pull-stage') {
             steps {
-                git branch: 'main', url: 'https://github.com/aniket-dudhe/jenkins-44.git'
+                git branch: 'main', url: 'https://github.com/aniket-dudhe/student-ui-app.git'
             }
         }
         stage('Build') {
