acae1361d27d8533347fd5e32be1ddcaa1d1433b
aniket-dudhe <aniketdudhe62@gmail.com>
2025-11-25 22:58:34 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -13,10 +13,7 @@ pipeline {
         }
         stage('test') {
             steps {
-               sh '''/opt/maven/bin/mvn sonar:sonar
--Dsonar.projectKey=sonar
--Dsonar.host.url=http://172.17.65.108:9000/
--Dsonar.login=sqa_11dd3211844b33ecc452ab9099b8c9283c3d8537'''
+        echo "ab"
             }
         }
         stage('deploy') {
