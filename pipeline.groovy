d585e8207f51805d6da895631e16b02595d48bd1
aniket-dudhe <aniketdudhe62@gmail.com>
2025-11-25 23:50:09 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -17,7 +17,7 @@
                   sh '''/opt/maven/bin/mvn sonar:sonar \\
                        -Dsonar.projectKey=sonar \\
                        -Dsonar.host.url=http://172.17.65.108:9000\\
-                       -Dsonar.login=11dd3211844b33ecc452ab9099b8c9283c3d8537'''
+                       -Dsonar.login=sqa_c5be7a4aaa200ef1731750b8e93b7c49acc40749'''
              }
          }
      }
