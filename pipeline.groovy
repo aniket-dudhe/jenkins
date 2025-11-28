88625ce62ef58f1ddf5090dd95f72ca019261cb1
aniket-dudhe <aniketdudhe62@gmail.com>
2025-11-28 16:12:20 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -11,7 +11,13 @@ pipeline {
                 sh 'mvn clean package'
             }
         }
-        
+        // stage('test') {
+        //     steps {
+        //         withSonarQubeEnv(installationName:'sonar',credentialsId: 'sonar-token') {
+        //                 sh '/opt/maven/bin/mvn sonar:sonar'
+        //            }   
+        //     }
+        // }
         //  stage('Quality-gate') {
         //     steps {
         //         timeout(10) {
@@ -20,86 +26,15 @@ pipeline {
         //         waitForQualityGate true
         //     }
         // }
-        stage('deploy') {
+         stage('s3-artifactory') {
             steps {
-                deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: 'tomcatcred', path: '', url: 'http://43.204.110.163:8080')], contextPath: '/', war: '**/*.war'
+                sh 'aws s3 cp target/studentapp-2.2-SNAPSHOT.war  s3://zhat12'
             }
         }
+        // stage('deploy') {
+        //     steps {
+        //         deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: 'tomcat-cred', path: '', url: 'http://35.78.123.34:8080')], contextPath: '/', war: '**/*.war'
+        //     }
+        // }
     }
 }
-
-
-
-
-
-// pipeline {
-//     agent {label 'slave'}
-//     stages {
-//         stage('pull-stage') {
-//             steps {
-//                 git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
-//             }
-//         }
-//         stage('Build') {
-//             steps {
-//                 sh '/opt/maven/bin/mvn clean package'
-//             }
-//         }
-//         stage('test') {
-//             steps {
-
-//                sh '''/opt/maven/bin/mvn sonar:sonar \\
-//                       -Dsonar.projectKey=studentapp \\
-//                       -Dsonar.host.url=http://18.180.215.234:9000 \\
-//                       -Dsonar.login=5fe5333d656b765e9b759cfb4a166b8594f4b779'''
-               
-//                 //     sh '/opt/maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://35.74.27.54:9000  -Dsonar.login=9c547e0733545b5a64a73f56e83c0169e6b5e1d7'
-//             }
-//         }
-//         stage('deploy') {
-//             steps {
-//                 echo "deploy sucess"
-//             }
-//         }
-//     }
-// }
-
-
-
-
-// pipeline {
-//     agent {label 'slave'}
-//     stages {
-//         stage('pull-stage') {
-//             steps {
-//                 git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
-//             }
-//         }
-//         stage('Build') {
-//             steps {
-//                 sh '/opt/maven/bin/mvn clean package'
-//             }
-//         }
-//         stage('test') {
-//             steps {
-//                 withSonarQubeEnv(installationName:'sonar',credentialsId: 'sonar-token') {
-//                          sh '/opt/maven/bin/mvn sonar:sonar'
-//                   }
-                 
-
-
-//             //   sh '''/opt/maven/bin/mvn sonar:sonar \\
-//             //           -Dsonar.projectKey=studentapp \\
-//             //           -Dsonar.host.url=http://18.180.215.234:9000 \\
-//             //           -Dsonar.login=5fe5333d656b765e9b759cfb4a166b8594f4b779'''
-               
-//                 //     sh '/opt/maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://35.74.27.54:9000  -Dsonar.login=9c547e0733545b5a64a73f56e83c0169e6b5e1d7'
-//             }
-//         }
-//         stage('deploy') {
-//             steps {
-//                 echo "deploy sucess"
-//             }
-//         }
-//     }
-// }
