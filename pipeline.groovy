eda578e2390f29f237e343ef52807d026e21d3be
aniket-dudhe <aniketdudhe62@gmail.com>
2025-11-30 10:46:06 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -18,6 +18,17 @@ pipeline {
         //            }   
         //     }
         // }
+            stage('test') {
+            steps {
+
+               sh '''mvn sonar:sonar
+                    -Dsonar.projectKey=sonar
+                    -Dsonar.host.url=http://3.109.202.95:9000
+                    -Dsonar.login=sqp_841640b096734c3f49eba5b4d64e8f894ebe0ada'''
+               
+                //     sh '/opt/maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://35.74.27.54:9000  -Dsonar.login=9c547e0733545b5a64a73f56e83c0169e6b5e1d7'
+            }
+        }
         //  stage('Quality-gate') {
         //     steps {
         //         timeout(10) {
@@ -31,10 +42,10 @@ pipeline {
         //         sh 'aws s3 cp target/studentapp-2.2-SNAPSHOT.war  s3://docker-buck-artifact-0099955'
         //     }
         // }
-        stage('deploy') {
-            steps {
-                deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: 'tomcatcred', path: '', url: 'http://15.206.211.208:8080/')], contextPath: '/', war: '**/*.war'
-            }
-        }
+        // stage('deploy') {
+        //     steps {
+        //         deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: 'tomcat-cred', path: '', url: 'http://35.78.123.34:8080')], contextPath: '/', war: '**/*.war'
+        //     }
+        // }
     }
 }
